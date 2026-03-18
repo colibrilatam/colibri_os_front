2835377b4f8383d6cf20c4da80d917a38cd21c6a
JoaquinGabriel17 <joaquingabriel3@hotmail.com>
2026-03-18 19:17:09 -0300

    estructura de carpetas

---
+++ client/src/app/dashboard/identidad/page.jsx
@@ -0,0 +1,8 @@
+export default function IdentidadPage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Identidad</h1>
+      <p>Contenido de Identidad</p>
+    </div>
+  );
+}
---
+++ client/src/app/dashboard/page.jsx
@@ -0,0 +1,24 @@
+export default function DashboardHome() {
+  return (
+    <div className="flex flex-col gap-6">
+      
+      {/* Saludo */}
+      <section className="bg-white p-6 rounded-2xl shadow">
+        <h1 className="text-2xl font-bold">
+          Hola 👋
+        </h1>
+        <p className="text-gray-600">
+          Este es tu resumen general
+        </p>
+      </section>
+
+      {/* Cards */}
+      <section className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+        <div className="bg-white p-4 rounded-xl shadow">Identidad</div>
+        <div className="bg-white p-4 rounded-xl shadow">Ruta de vuelo</div>
+        <div className="bg-white p-4 rounded-xl shadow">Competencias</div>
+      </section>
+
+    </div>
+  );
+}
