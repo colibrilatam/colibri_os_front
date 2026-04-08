b4d61c77cbc7d9d93ceceaa6282c530a3dcbea53
JoaquinGabriel17 <joaquingabriel3@hotmail.com>
2026-04-08 11:06:22 -0300

    ajustes a imagen de nft

--- client/src/components/señal/NftAvatar.jsx
+++ client/src/components/señal/NftAvatar.jsx
@@ -1,13 +1,13 @@
-
+import { project } from "@/lib/mock-data";
 
 export default function NftAvatar({ size = "sm" }) {
   const isLarge = size === "lg";
 
   return (
-      <div className={`${isLarge ? "h-64 w-64" : "h-14 w-14"}   relative flex items-center justify-center `}>
+      <div className={`${isLarge ? "h-64 w-64" : "h-14 w-14"} rounded-full bg-white  relative flex items-center justify-center `}>
         <img 
           className="w-full h-full object-contain rounded-full"
-          src="/T4.png" 
+          src={`${project.tramoCode}.png`}
           alt="NFT Avatar"
         />
       </div>
