ecd5aa43c5c7278c320e59580e8ac03d134d5f61
JoaquinGabriel17 <joaquingabriel3@hotmail.com>
2026-04-25 21:29:47 -0300

    fix en layout project

--- client/src/app/proyecto/layout.jsx
+++ client/src/app/proyecto/layout.jsx
@@ -13,8 +13,7 @@ export default async function ProjectLayout({ children }){
     // DEMO
     const isDemo = true
 
-
-    if(user.email === 'ana@colibri.com') {
+    if(user && user.email && user.email  === 'ana@colibri.com' && isDemo) {
         return(
             <>
             {children}
