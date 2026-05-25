da6b5be04d4b9f0dd2d09a9d286a366232758c35
JoaquinGabriel17 <joaquingabriel3@hotmail.com>
2026-05-25 14:14:39 -0300

    fix

--- client/src/app/dashboard/[id]/trayectoria/NewPage.jsx
+++ client/src/app/dashboard/[id]/trayectoria/NewPage.jsx
@@ -211,7 +211,6 @@ export default function NewTrayectoria() {
 
       if (updatePacError) {
         console.log(updatePacError)
-        return;
       };
       getPacsInfo();
       setIsPacCompleted(true);
