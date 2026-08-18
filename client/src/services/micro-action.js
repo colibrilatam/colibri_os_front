18cd20f55044e46ad1a854de8d709490e84c0e96
Franz Duran <franzduran.devfs@gmail.com>
2026-08-18 14:34:17 -0300

    page evaluation

--- client/src/services/micro-action.js
+++ client/src/services/micro-action.js
@@ -1,16 +1,31 @@
 import { apiClient } from '@/lib/api';
 
 export const microActionService = {
+  getAll: async (params = {}) => {
+    const response = await apiClient.get('/micro-action-instances/all', {
+      params,
+    });
+
+    return response.data;
+  },
+
   createVersion: async (instanceId, { file, executionNotes }) => {
     const body = new FormData();
     body.append('file', file);
-    if (executionNotes !== null && executionNotes !== undefined && executionNotes !== '') {
+
+    if (
+      executionNotes !== null &&
+      executionNotes !== undefined &&
+      executionNotes !== ''
+    ) {
       body.append('executionNotes', executionNotes);
     }
+
     const response = await apiClient.post(
       `/micro-action-instances/${instanceId}/versions`,
       body,
     );
+
     return response.data;
   },
-}
+};
